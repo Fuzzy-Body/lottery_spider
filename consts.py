6104ae7a12403469d1906bc73c7f331dce1f80e2
gong_ting <gongting88@yeah.net>
2017-08-13 15:46:39 +0800

    asian match done

--- consts.py
+++ consts.py
@@ -4,3 +4,11 @@ MATCH_LIST = 'http://i.sporttery.cn/api/fb_match_info/get_matches?f_callback=get
 
 MATCH_INFO = 'http://i.sporttery.cn/api/fb_match_info/get_match_info?mid={mid}'\
     '&f_callback=getMatchInfo&_={ts}'
+
+# Asian Handicap 亚盘
+
+ASIAN_MATCH = 'http://i.sporttery.cn/api/fb_match_info/get_asia/?f_callback=get_match_asia'\
+    '&mid={mid}&_={ts}'
+
+
+MACAO_ID = '229'
--- main.py
+++ main.py
@@ -26,20 +26,52 @@ def load(filename='data/match_list.pkl'):
     return data
 
 
-def get_match_list():
+def _request(url, to_replace='', **kwargs):
     ts = int(time.time() * 1000)
-    match_list_url = MATCH_LIST.format(ts=ts)
-    response = requests.get(match_list_url, headers=HEADER)
+    url = url.format(ts=ts, **kwargs)
+    logger.info('get url: %s' % url)
+    response = requests.get(url, headers=HEADER)
     if response.status_code != 200:
-        logger.error('get_match_list error, code: %s' % response.status_code)
+        logger.error('get url %s error, code: %s' % (url, response.status_code))
         logger.error(response.text)
         raise
-    data = response.text.replace('getMatchList', '')
+    data = response.text.replace(to_replace, '')
     data = data.strip('();')
     data = json.loads(data)
+    return data
+
+
+def get_match_list():
+    data = _request(MATCH_LIST, to_replace='getMatchList')
     save(data)
 
 
+def get_match_info(mid):
+    # 获取比赛时间
+    data = _request(MATCH_INFO, to_replace='getMatchInfo', mid=mid)
+
+    return data['result']['date_cn'] + ' ' + data['result']['time_cn']
+
+
+def get_asian_match(mid):
+    # 获取亚盘澳门的信息
+    data = _request(ASIAN_MATCH, to_replace='get_match_asia', mid=mid)
+    for item in data['result']:
+        if item['id'] == MACAO_ID:
+            return {
+                'home': item['h'],
+                'drew': item['d'],
+                'away': item['a'],
+            }
+    raise ValueError('没有找到澳门盘口')
+
+
+def test():
+    # get_match_list()
+    # print(load())
+    # print(get_match_info('96746'))
+    print(get_asian_match('96746'))
+
+
 if __name__ == '__main__':
-    get_match_list()
-    print(load())
+    test()
